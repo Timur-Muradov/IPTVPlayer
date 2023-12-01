e4bf47172ee52483f4467287887a204f78706c50
Timur-Muradov <timur.muradov.1996@mail.ru>
2023-12-01 22:57:17 +0300

    Implemented ChannelViewModel.

---
+++ IPTVPlayer/UI/Models/ChannelViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  ChannelViewModel.swift
+//  IPTVPlayer
+//
+//  Created by Тимур Мурадов on 01.12.2023.
+//
+
+import Foundation
+
+struct ChannelViewModel {
+    let logo: URL
+    let title: String
+    let currentShow: String
+    let playlistURL: URL
+    let isFavorite: Bool
+}
