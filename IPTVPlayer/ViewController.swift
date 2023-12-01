873148f4dd66db52e9602f69050731700f3da83d
Timur-Muradov <timur.muradov.1996@mail.ru>
2023-12-01 20:11:35 +0300

    Initial Commit

---
+++ IPTVPlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IPTVPlayer
+//
+//  Created by Тимур Мурадов on 01.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
